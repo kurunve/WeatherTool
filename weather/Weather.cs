ac2e4773ce4c129cec0008f568ffff95790f7a15
Vova <inspironty@yahoo.com>
2012-08-25 19:24:33 +0300

    First Commit Of Empty files

---
+++ weather/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace weather
+{
+    public class XMLfromsite
+    {
+        public static void getWeather(string city)
+        {
+            try
+            {
+                XmlDocument weather = new XmlDocument();
+                weather.Load(string.Format("http://www.google.com/ig/api?weather={0}", city));
+
+            }
+            catch
+            {
+
+                System.Console.Beep();
+            }
+        }
+
+    }
+}
