d4a52eeb83a1bda3de131044d46ccc7d2e7e3cd8
kurunve <VolodymyrAntonuk@gmail.com>
2013-02-14 18:11:43 +0200

    FirstCommit

---
+++ weather/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weather
+{
+    static class Constants
+    {
+        public static string WeatherApiKey = "80f4a8ea1a141657131402";
+
+    }
+}
